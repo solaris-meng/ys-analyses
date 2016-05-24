7eb1dff4234a3937dba2a634eb24be19bd5c36a1
Meng An <isolate000@gmail.com>
2016-05-24 14:24:10 +0800

    add tag score method

--- pvtrace_preprocess_score.py
+++ pvtrace_preprocess_score.py
@@ -1,6 +1,9 @@
+# encoding=utf-8
 import requests
 import json
 import logging
+import jieba
+import os
 
 from pyspark import SparkContext
 
@@ -38,15 +41,18 @@ def score_trace(file_dir, file_name):
 
     sd = score_data.collect()
     for each_s in sd:
-        line_str = ''
-        for each_p in each_s:
-            line_str = line_str + '\t' + each_p
+        line_str = each_s[0]
+        p_count = len(each_s)
+        for i in range(1, p_count):
+            line_str = line_str + '\t' + each_s[i]
         line_str = line_str + '\n'
         output_file.write(line_str)
 
     output_file.close()
-    return output_file_url
+    
+    sc.stop()
     print('---------------- SCORE_TRACE, Done - %s -------------------' % file_dir)
+    return output_file_url
 
 def trace_line_score(line):
     scores = []
@@ -71,9 +77,78 @@ def trace_line_score(line):
     
     return scores
 
+def score_tag(score_file_url):
+    
+    sc = SparkContext("local", "Simple App") 
+    trace_score_data = sc.textFile(score_file_url)
+    tag_score_data = trace_score_data.map(lambda line: line.split("\t")).flatMap(sword_jieba_tag).groupByKey().mapValues(list).map(tag_score_combine)
+
+    """ 
+    count = tag_score_data.count()
+    print('---------------- SCORE_TAG, Count - %d -------------------' % count)
+    for each in tag_score_data.collect():
+        print(each)
+    """
+
+    file_name = os.path.basename(score_file_url)
+    output_file_url = '/root/zhaoxq/analyze_file/' + file_name + '-bytags'
+    output_file = open(output_file_url, 'w')
+    
+    
+    tsd = tag_score_data.collect()
+    for each_t in tsd:
+        line_str = each_t[0] + '\t%d' % each_t[1]
+        for  each_page in each_t[2]:
+            line_str = line_str + '\t%f' % each_page
+        line_str = line_str + '\n'
+        output_file.write(line_str)
+
+    output_file.close()
+    sc.stop()
+    print('---------------- SCORE_TAG, Done - %s -------------------' % output_file_url)
+    return output_file_url
+
+def sword_jieba_tag(line):
+    tag_scores = []
+    page_count = len(line) - 1 
+    sword = line[0]
+    seg_list = jieba.cut(sword, cut_all=False)
+    for each_seg in seg_list:
+        tag_line = []
+        #tag_line.append(each_seg)
+        for i in range(1 , page_count + 1):
+            tag_line.append(line[i])
+        tag_kv = (each_seg, tag_line)
+        tag_scores.append(tag_kv)
+
+    return tag_scores
+
+def tag_score_combine(line):
+    
+    tag = line[0]
+    scores = line[1]
+    count = len(scores)
+
+    total_scores = []
+    for i in range(count):
+        page_count = len(scores[i])
+        page_score = scores[i]
+        for j in range(page_count):
+            if i == 0:
+                total_scores.append(float(page_score[j]))
+            else:
+                total_scores[j] += float(page_score[j])
+
+    for k in range(len(total_scores)):
+        total_scores[k] = total_scores[k] / float(count)
+
+    return (tag, count, total_scores)
+
+
 if __name__ == '__main__':
 
     print("score_trace, started")
     file_dir = '/root/zhaoxq/analyze_file'
     file_name = 'gaode-lp2-2016-5-11'
     score_file_url = score_trace(file_dir, file_name)
+    tag_file_url = score_tag(score_file_url)
